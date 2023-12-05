c5d46acd5d4e7d27e3b1c25097df193646ddf4da
krzymich24 <krzysztof.michalak1999@gmail.com>
2023-12-05 19:23:42 +0100

    test content of map creator program

---
+++ firewall-rust/map-creator/main.rs
@@ -0,0 +1,46 @@
+use aya::{
+    include_bytes_aligned,
+    maps::HashMap,
+    Bpf,
+};
+use log::{info, warn};
+use aya_log::BpfLogger;
+use std::net::Ipv4Addr;
+use tokio::signal;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+
+    // Load the eBPF object file at compile-time
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../../firewall/target/bpfel-unknown-none/debug/firewall"
+    ))?;
+
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../../firewall/target/bpfel-unknown-none/release/firewall"
+    ))?;
+
+    // Initialize the eBPF logger
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+
+    // Create a rule map
+    let mut rule_map: HashMap<_, u32, u32> = HashMap::try_from(bpf.map_mut("rule").unwrap())?;
+
+    // Insert a sample rule into the map
+    let block_addr: u32 = Ipv4Addr::new(1, 1, 1, 1).into();
+    rule_map.insert(block_addr, 0, 0)?;
+
+    info!("Rule map created. Waiting for Ctrl-C...");
+
+    // Wait for Ctrl+C signal
+    signal::ctrl_c().await?;
+
+    info!("Exiting...");
+
+    Ok(())
+}
