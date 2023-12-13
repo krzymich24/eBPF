678ca86db005a3b1ee7c381397cb790694861d22
krzymich24 <krzysztof.michalak1999@gmail.com>
2023-12-13 17:07:50 +0100

    still working with blocking ICMP

--- firewall-c/firewall_kern.c
+++ firewall-c/firewall_kern.c
@@ -11,7 +11,7 @@
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
-    __type(key, struct rule);
+    __type(key, __u32);
     __type(value, struct rule);
     __uint(max_entries, 1024);
     __uint(pinning, LIBBPF_PIN_BY_NAME);
@@ -55,14 +55,41 @@ int bpf_program1(struct xdp_md *ctx) {
     h_proto = eth->h_proto;
 
     if (h_proto == htons(ETH_P_IP)) { 
+        
         struct iphdr *ip = data + sizeof(struct ethhdr);
         ip_protocol = parse_ipv4(data, nh_off, data_end);
-         if (ip_protocol == IPPROTO_ICMP) {
+
+        if (ip_protocol == IPPROTO_ICMP) {
             struct iphdr *iph = data + nh_off;
             uint32_t src_ip = iph->saddr;
 			uint32_t dest_ip = iph->daddr;
-            return XDP_DROP;
-         }
+
+            struct rule *rule_entry;
+            __u32 key = 0;  
+
+            for (int i = 0; i < 1024; i++) {
+                rule_entry = (struct rule *)bpf_map_lookup_elem(&rule_map, &key);
+
+                if (rule_entry) {
+                    int32_t rule_protocol = rule_entry->protocol;
+
+                    if (rule_protocol == 1) {
+                        uint32_t rule_src_ip = rule_entry->source_ip;
+                        uint32_t rule_dest_ip = rule_entry->dest_ip;
+
+                        if (src_ip == rule_src_ip && dest_ip == rule_dest_ip) {
+                            return XDP_DROP; 
+                        }
+
+                    }
+
+                    key++;
+                }
+
+            }
+
+        }
+
         return XDP_PASS;
     } else if (h_proto == htons(ETH_P_IPV6)){
         struct ipv6hdr *ipv6 = data + sizeof(struct ethhdr);
